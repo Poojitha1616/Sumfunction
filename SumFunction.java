c2834f00d6043110c32b08de25b5ba481d089e38
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 23:28:33 +0530

    Add files via upload
    this program displays the sum of two numbers

---
+++ SumFunction.java
@@ -0,0 +1,13 @@
+
+public class SumFunction {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        int sum = calculateSum(n1, n2);
+        System.out.println("The sum of " + n1 + " and " + n2 + " is: " + sum);
+    }
+
+    public static int calculateSum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
